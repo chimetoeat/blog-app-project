5425402f881b04d2318c14e987024b9b7878f2fb
Vince Ivan Minez <banongofficial28@gmail.com>
2023-08-24 02:00:37 +0800

    Added Registration Backend Functionality

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,24 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import axios from 'axios';
+import { useState } from 'react';
 
 function Register() {
+    const [username, setUsername] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        axios.post('http://localhost:3001/register', {username, email, password})
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
+
+
   return (
     <div className='container'>
         <div className='form'>
             <h2>Sign Up</h2>
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
                 <div className='field'>
                     <label htmlFor="name">Username:</label>
-                    <input type="text" />
+                    <input type="text" onChange={e => setUsername(e.target.value)}/>
                 </div>
                 <div className='field'>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" />
+                    <input type="email" onChange={e => setEmail(e.target.value)}/>
                 </div>
                 <div className='field'>
                     <label htmlFor="password">Password:</label>
-                    <input type="password" />
+                    <input type="password" onChange={e => setPassword(e.target.value)}/>
                 </div>
                 <button>Sign up</button>
             </form>
--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ const jwt = require("jsonwebtoken")
 const cookieParser = require("cookie-parser")
 const multer = require("multer")
 const path = require("path")
-const { log } = require("console")
+const UserModel = require('./models/UserModel')
 
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173/"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
@@ -19,6 +19,18 @@ app.use(cookieParser())
 
 
 
+mongoose.connect('mongodb://127.0.0.1:27017/blog')
+
+app.post('/register', (req, res) => {
+    const {username, email, password} = req.body;
+    bcrypt.hash(password, 10)
+    .then(hash => {
+        UserModel.create({username, email, password: hash})
+        .then(user => res.json(user))
+        .catch(err => res.json(err))
+    }).catch(err => console.log(err))
+
+})
 
 app.listen(3001, () => {
     console.log("Server is running at port 3001.")
